2e3f21d18af92c706cd271b93bfc1717d947373b
Délita Makanda <delita.makanda@gmail.com>
2024-11-01 23:08:32 +0100

    fix(products): fix tests

--- src/app/core/services/product.service.spec.ts
+++ src/app/core/services/product.service.spec.ts
@@ -28,17 +28,9 @@ describe('ProductService', () => {
   });
 
   it('should get all products', () => {
-    const mockProducts = {
-      results: {
-        results: {
-          results: [
-            { uuid: '1', name: 'Product 1', price: 100, category: ['Electronics'] },
-            { uuid: '2', name: 'Product 2', price: 200, category: ['Computers'] },
-            { uuid: '3', name: 'Product 3', price: 300, category: ['Clothing']  },
-          ]
-        }
-      }
-    } as any;
+    const mockProducts = [{ uuid: '1', name: 'Product 1', price: 100, category: ['Electronics'] },
+      { uuid: '2', name: 'Product 2', price: 200, category: ['Computers'] },
+      { uuid: '3', name: 'Product 3', price: 300, category: ['Clothing']  }] as any;
 
     service.getAll().subscribe((products) => {
       expect(products).toEqual(mockProducts);
@@ -46,6 +38,6 @@ describe('ProductService', () => {
 
     const req = httpMock.expectOne(`${apiUrl}/store/product/?ordering=-created_at`);
     expect(req.request.method).toBe('GET');
-    req.flush({ results: mockProducts.results });
+    req.flush({ results: mockProducts });
   });
 });
