8ac8c1b1c703ff1e43191ba9339d0cd98f879991
Délita Makanda <delita.makanda@gmail.com>
2024-11-03 21:32:00 +0100

    feat(login): add login feature

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
+import { FormsModule, NgForm} from "@angular/forms";
+import {KeyValuePipe, NgFor, NgStyle} from "@angular/common";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    FormsModule
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
-
+  login(form: NgForm): void {
+    if (form.invalid) {
+      return;
+    }
+    console.log(form.value);
+  }
 }
