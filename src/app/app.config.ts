1889e855220ecfa6989c0e0618a77ac9d8dc9cee
Délita Makanda <delita.makanda@gmail.com>
2024-12-31 22:37:16 +0100

    optimize perf

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,20 +1,19 @@
-import { ApplicationConfig, inject, LOCALE_ID, InjectionToken } from "@angular/core";
+import { ApplicationConfig, inject, LOCALE_ID } from "@angular/core";
 import { routes } from "./app.routes";
-import { provideRouter } from '@angular/router';
+import {PreloadAllModules, provideRouter, withInMemoryScrolling, withPreloading} from '@angular/router';
 import { registerLocaleData } from "@angular/common";
 import localeFr from "@angular/common/locales/fr";
 import { AppConfigService } from "./core/services/app-config.service";
 import { LoggerService, RemoteLoggerService } from "./core/services/logger.service";
 import { API_URL, APP_ENVIRONMENT } from "./constants";
 import { provideHttpClient } from "@angular/common/http";
-import { provideHttpClientTesting } from "@angular/common/http/testing";
 
 registerLocaleData(localeFr);
 
 
 export const appConfig: ApplicationConfig = {
     providers: [
-        provideRouter(routes),
+        provideRouter(routes, withPreloading(PreloadAllModules), withInMemoryScrolling({ scrollPositionRestoration: 'top'  }  )),
         { provide: LOCALE_ID, useValue: 'fr' },
         { provide: LoggerService, useClass: RemoteLoggerService  },
         { provide: API_URL, useValue: 'https://merchstoreapi.applikuapp.com/api' },
