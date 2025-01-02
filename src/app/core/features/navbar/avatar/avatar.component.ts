4f1484c156fdf6ea64a697074a7656b2169e8996
Délita Makanda <delita.makanda@gmail.com>
2025-01-02 16:58:06 +0100

    protect routes

--- src/app/core/features/navbar/avatar/avatar.component.ts
+++ src/app/core/features/navbar/avatar/avatar.component.ts
@@ -1,12 +1,14 @@
 import { NgStyle } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { SharedModule } from 'src/app/shared/shared.module';
+import {RouterLink} from "@angular/router";
 
 @Component({
     selector: 'app-avatar',
     imports: [
         SharedModule,
         NgStyle,
+      RouterLink,
     ],
     templateUrl: './avatar.component.html',
     styleUrl: './avatar.component.scss'
--- src/app/core/features/navbar/navbar.component.ts
+++ src/app/core/features/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import {CommonModule, NgIf} from '@angular/common';
+import {Component, inject} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { RouterLink } from '@angular/router';
 import { AvatarComponent } from './avatar/avatar.component';
 import {SearchBarComponent} from "../search-bar/search-bar/search-bar.component";
+import {AuthService} from "../../services/auth.service";
 
 @Component({
     selector: 'app-navbar',
@@ -13,12 +14,14 @@ import {SearchBarComponent} from "../search-bar/search-bar/search-bar.component"
         FormsModule,
         AvatarComponent,
         SearchBarComponent,
+      NgIf,
     ],
     templateUrl: './navbar.component.html',
     styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   searchInput = '';
+  authService = inject(AuthService);
 
   search(): void {
     console.log(`Searching for: ${this.searchInput}`);
