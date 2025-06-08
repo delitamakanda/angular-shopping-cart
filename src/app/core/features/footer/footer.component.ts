18bc32c19088b424876890c1f7af7e36708263ca
Délita Makanda <delita.makanda@gmail.com>
2025-06-09 00:11:43 +0200

    add footer

---
+++ src/app/core/features/footer/footer.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  imports: [],
+  template: `
+    <footer>
+      <p>{{ credits }}. All rights reserved.</p>
+    </footer>`,
+  styleUrl: './footer.component.scss'
+})
+export class FooterComponent {
+
+  protected readonly credits = new Date().getFullYear() + ' - Angular Shopping Cart';
+}
