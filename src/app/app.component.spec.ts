daefde0bd406537a0582303da66b8910d6003f3c
Délita Makanda <delita.makanda@gmail.com>
2025-06-09 22:35:08 +0200

    fix test

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,22 +1,24 @@
-import {ComponentFixture, TestBed, waitForAsync} from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 import { AppComponent } from './app.component';
 import {provideRouter} from "@angular/router";
 import {routes} from "./app.routes";
 import {provideHttpClient} from "@angular/common/http";
 import {HttpTestingController, provideHttpClientTesting} from "@angular/common/http/testing";
-import {ProductService} from "./core/services/product.service";
 import {API_URL} from "./constants";
 import {NavbarComponent} from "./core/features/navbar/navbar.component";
+import {FooterComponent} from "./core/features/footer/footer.component";
 
 describe('AppComponent', () => {
   let fixture: ComponentFixture<AppComponent>;
   let component: AppComponent;
+  let httpMock: HttpTestingController;
 
-  beforeEach(waitForAsync(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async() => {
+    await TestBed.configureTestingModule({
       imports: [
         AppComponent, // Import the standalone component
-        NavbarComponent, // Mock child components if needed
+        NavbarComponent,
+        FooterComponent// Mock child components if needed
       ],
       providers: [
         provideRouter(routes),
@@ -24,11 +26,13 @@ describe('AppComponent', () => {
         provideHttpClientTesting(),
         {provide: API_URL, useValue: 'https://example.com/api'}
       ]
-    }).compileComponents().then(() => {
-      fixture = TestBed.createComponent(AppComponent);
-      component = fixture.componentInstance;
-    });
-  }));
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+    fixture.detectChanges();
+  });
 
   it('should create the app', () => {
     expect(component).toBeTruthy();
