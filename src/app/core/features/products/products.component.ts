89ad3eb0d534b388bd0d0860ab185ed50e8712d6
Délita Makanda <delita.makanda@gmail.com>
2025-06-10 22:45:32 +0200

    responsive grid list

--- src/app/core/features/products/products.component.ts
+++ src/app/core/features/products/products.component.ts
@@ -1,4 +1,4 @@
-import {Component, effect, inject } from '@angular/core';
+import {Component, effect, inject, OnInit} from '@angular/core';
 import { CardComponent } from './card/card.component';
 import { SharedModule } from 'src/app/shared/shared.module';
 import { ProductService } from '../../services/product.service';
@@ -8,6 +8,8 @@ import {SearchBarComponent} from "../search-bar/search-bar/search-bar.component"
 import {MatProgressSpinnerModule} from "@angular/material/progress-spinner";
 import {CategoryListComponent} from "./category-list/category-list.component";
 import {MatGridListModule} from "@angular/material/grid-list";
+import {BreakpointObserver, Breakpoints} from "@angular/cdk/layout";
+import {CommonObservableDestruction} from "../../../shared/helpers/common.observable";
 
 @Component({
     selector: 'app-products',
@@ -23,18 +25,19 @@ import {MatGridListModule} from "@angular/material/grid-list";
     templateUrl: './products.component.html',
     styleUrl: './products.component.scss'
 })
-export class ProductsComponent {
+export class ProductsComponent extends CommonObservableDestruction implements OnInit {
   productService = inject(ProductService);
   products = this.productService.products;
   isLoading = false;
   // filterCategory = '';
   // propCategories = ['Electronics', 'Computers', 'Clothing', 'Accessories', 'Smartphones'];
-
+  breakpointsCols = 4
   get allProducts(): Product[] {
     return this.products();
   }
 
-  constructor() {
+  constructor(private breakpointsObs: BreakpointObserver) {
+    super();
     effect(() => {
       this.isLoading = true;
       this.productService.getAll()
@@ -43,6 +46,26 @@ export class ProductsComponent {
         this.isLoading = false;
       }, 1000);
     });
+  }
+
+  ngOnInit() : void{
+    this.breakpointsObs.observe([
+      Breakpoints.XSmall,
+      Breakpoints.Small,
+      Breakpoints.Medium,
+      Breakpoints.Large,
+      Breakpoints.XLarge,
+    ]).pipe(this.untilDestroyed()).subscribe((result) => {
+      if (result.breakpoints[Breakpoints.XSmall]) {
+        this.breakpointsCols = 1;
+      } else if (result.breakpoints[Breakpoints.Small]) {
+        this.breakpointsCols = 2;
+      } else if (result.breakpoints[Breakpoints.Medium]) {
+        this.breakpointsCols = 3;
+      } else {
+        this.breakpointsCols = 4;
+      }
+    })
   }
 
   removeProduct(uuid: string): void {
