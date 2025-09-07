12a72433fc91648dd1c882f8e7c780271f4362bf
Délita Makanda <delita.makanda@gmail.com>
2025-09-07 22:59:12 +0200

    add static pages links to footer with responsive styles

--- src/app/core/features/footer/footer.component.ts
+++ src/app/core/features/footer/footer.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   template: `
     <footer>
+      <ul>
+        @for(
+          entry of staticPages; track entry
+          ) {
+          <li>
+            <a [routerLink]="['/static-pages', entry[0]]">{{ entry[1] }}</a>
+          </li>
+        }
+      </ul>
       <p>{{ credits }}. All rights reserved.</p>
     </footer>`,
   styleUrl: './footer.component.scss'
@@ -12,4 +24,23 @@ import { Component } from '@angular/core';
 export class FooterComponent {
 
   readonly credits = new Date().getFullYear() + ' - Angular Shopping Cart';
+  readonly staticPagesDictionary: Record<string, string> = {
+    'legal-notice': 'Mentions légales',
+    'terms-of-use': 'Conditions d\'utilisation',
+    'privacy-policy': 'Politique de confidentialité',
+    'terms-of-sale': 'Conditions générales de vente',
+    'cookies-policy': 'Politique de cookies',
+  }
+
+  get staticPages(): [string, string][] {
+    return Object.entries(this.formatStaticPageObject(this.staticPagesDictionary));
+  }
+
+   formatStaticPageObject(staticPages: Record<string, string>) : Record<string, string>  {
+    const formattedPages: Record<string, string> = {};
+    for (const [key, value] of Object.entries(staticPages)) {
+      formattedPages[key.toLowerCase()] = value;
+    }
+    return formattedPages;
+  }
 }
