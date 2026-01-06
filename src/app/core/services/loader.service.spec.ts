a705c4fcc3fa6a184f68b7dd3c437d24d1df1c67
Délita Makanda <delita.makanda@gmail.com>
2026-01-06 21:12:58 +0100

    add unit test to verify global loader initialization during navigation events

--- src/app/core/services/loader.service.spec.ts
+++ src/app/core/services/loader.service.spec.ts
@@ -1,18 +1,27 @@
 import { TestBed } from '@angular/core/testing';
 
 import { LoaderService } from './loader.service';
+import {NavigationStart, Router} from "@angular/router";
+import {Subject} from "rxjs";
 
 describe('LoaderService', () => {
   let service: LoaderService;
+  let router: Router
+  let routerEventSub: Subject<any>
 
   beforeEach(() => {
+    routerEventSub = new Subject<any>();
     TestBed.configureTestingModule({
-      providers: [],
+      providers: [LoaderService, {
+        provide: Router,
+        useValue: { events: routerEventSub.asObservable() }
+      }],
       imports: [],
       declarations: [],
       schemas: []
     });
     service = TestBed.inject(LoaderService);
+    router = TestBed.inject(Router);
   });
 
   it('should be created', () => {
@@ -20,7 +29,9 @@ describe('LoaderService', () => {
   });
 
   it('should initialize loader', () => {
+    spyOn(service as any, 'createOverlay');
     service.initializeLoader();
-    expect(service.overlayRef).toBeTruthy();
+    routerEventSub.next(new NavigationStart(1, '/'));
+    expect((service as any).createOverlay).toHaveBeenCalled();
   });
 });
