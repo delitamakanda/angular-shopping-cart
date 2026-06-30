408eedf1aae396677b68ba7b1fc32245b65bed94
Délita Makanda <delitamakanda@users.noreply.github.com>
2026-06-30 15:30:38 +0200

    refactor: simplify cart component change detection

--- src/app/core/features/cart/cart.component.ts
+++ src/app/core/features/cart/cart.component.ts
@@ -1,9 +1,9 @@
-import {Component, effect, inject, ChangeDetectionStrategy} from '@angular/core';
-import {CartService} from "../../services/cart.service";
-import {CurrencyPipe, NgOptimizedImage} from "@angular/common";
-import {RouterLink} from "@angular/router";
-import {ConfirmDirective} from "../../../shared/directives/confirm.directive";
-import {CartItem} from "../products/card/interfaces";
+import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import { CurrencyPipe, NgOptimizedImage } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { CartService } from '../../services/cart.service';
+import { ConfirmDirective } from '../../../shared/directives/confirm.directive';
+import { CartItem } from '../../interfaces';
 
 @Component({
   selector: 'app-cart',
@@ -14,25 +14,20 @@ import {CartItem} from "../products/card/interfaces";
     ConfirmDirective,
   ],
   templateUrl: './cart.component.html',
-  changeDetection: ChangeDetectionStrategy.Eager,
-  styleUrl: './cart.component.scss'
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './cart.component.scss',
 })
 export class CartComponent {
-  cartService = inject(CartService);
-  cartItemsWithQuantity = this.cartService.cartItemsWithQuantity;
-  cartItemsTotalPrice = this.cartService.cartItemsTotalPrice;
-  base64Background = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5'
+  readonly cartService = inject(CartService);
+  readonly cartItemsWithQuantity = this.cartService.cartItemsWithQuantity;
+  readonly cartItemsTotalPrice = this.cartService.cartItemsTotalPrice;
+  readonly base64Background = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5';
 
   get cartItems(): CartItem[] {
     return this.cartItemsWithQuantity();
   }
+
   get totalCartPrice(): number {
     return this.cartItemsTotalPrice();
   }
-  constructor() {
-    effect(() => {
-      this.cartService.getCartFromLocalStorage();
-    })
-  }
-
 }
