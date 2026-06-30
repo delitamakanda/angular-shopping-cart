a90272cd08939aba498708a353d221a9cc2b2d91
Délita Makanda <delitamakanda@users.noreply.github.com>
2026-06-30 15:31:13 +0200

    refactor: clean product card component imports

--- src/app/core/features/products/card/card.component.ts
+++ src/app/core/features/products/card/card.component.ts
@@ -1,43 +1,39 @@
-import {Component, EventEmitter, Input, Output, ChangeDetectionStrategy} from '@angular/core';
-import { Product } from 'src/app/core/interfaces/product.interface';
-import { ProductCardComponent } from '../product-card/product-card.component';
-import { CommonModule, CurrencyPipe, IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
-import { SharedModule } from 'src/app/shared/shared.module';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
+import { CurrencyPipe, IMAGE_CONFIG, NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
-import {FormsModule} from "@angular/forms";
+import { FormsModule } from '@angular/forms';
+import { Product } from 'src/app/core/interfaces/product.interface';
+import { SharedModule } from 'src/app/shared/shared.module';
+import { ProductCardComponent } from '../product-card/product-card.component';
 
 @Component({
-    selector: 'app-card',
-    providers: [
-        {
-            provide: IMAGE_CONFIG,
-            useValue: {
-                placeHolderResolution: 40,
-            }
-        }
-    ],
-    imports: [
-        ProductCardComponent,
-        CurrencyPipe,
-        SharedModule,
-        NgOptimizedImage,
-      FormsModule,
-      CommonModule,
-        RouterLink,
-        NgOptimizedImage,
-    ],
-    templateUrl: './card.component.html',
-    changeDetection: ChangeDetectionStrategy.Eager,
-    styleUrl: './card.component.scss'
+  selector: 'app-card',
+  providers: [
+    {
+      provide: IMAGE_CONFIG,
+      useValue: {
+        placeHolderResolution: 40,
+      },
+    },
+  ],
+  imports: [
+    ProductCardComponent,
+    CurrencyPipe,
+    SharedModule,
+    NgOptimizedImage,
+    FormsModule,
+    RouterLink,
+  ],
+  templateUrl: './card.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  styleUrl: './card.component.scss',
 })
 export class CardComponent {
-  @Input() product: Product = {} as Product;
+  @Input({ required: true }) product!: Product;
   @Output() deleteProduct = new EventEmitter<string>();
-  userIsAdmin = false;
-  base64Background: string;
-  constructor() {
-    this.base64Background = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5v'
-  }
+
+  readonly userIsAdmin = false;
+  readonly base64Background = 'data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5v';
 
   handleDeleteProduct(): void {
     this.deleteProduct.emit(this.product.uuid);
