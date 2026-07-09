60052a990c38ed80dcc4c91ebc0b5923c6d76f5a
Délita MAKANDA <delita.makanda@axivit.com>
2026-07-09 16:53:35 +0200

    feat: add supported locales and default locale constants

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -2,3 +2,7 @@ import { InjectionToken } from "@angular/core";
 
 export const API_URL = new InjectionToken<string>('API_URL');
 export const APP_ENVIRONMENT = new InjectionToken<string>('APP_ENVIRONMENT');
+
+export const SUPPORTED_LOCALES = ['fr', 'en', 'ko'] as const;
+export type SupportedLocale = typeof SUPPORTED_LOCALES[number];
+export const DEFAULT_LOCALE: SupportedLocale = 'fr';
