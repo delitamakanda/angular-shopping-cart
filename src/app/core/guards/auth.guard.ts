bab25c6139dbda0a7e2d1b003522f61bce634497
Délita MAKANDA <delita.makanda@axivit.com>
2026-07-09 16:53:46 +0200

    feat: update auth guard to navigate to locale-specific login route

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,13 +2,14 @@ import { CanActivateFn } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 import { inject } from "@angular/core";
 import { Router } from '@angular/router';
+import { extractLocale } from '../services/locale.service';
 
 export const authGuard: CanActivateFn = (route, state) => {
   const authService = inject(AuthService);
   const router = inject(Router);
 
   if (!authService.isAuthenticated) {
-    router.navigate(['/login']);
+    router.navigate(['/', extractLocale(state.url), 'login']);
     return false;
   }
   return true;
